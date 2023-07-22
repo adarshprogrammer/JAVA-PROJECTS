fbb03a7e3321bfeee3564e3bdd5aede4b607b879
ADARSH KUMAR <100017142+adarshprogrammer@users.noreply.github.com>
2023-07-22 14:27:56 +0530

    Add files via upload

---
+++ web19/src/java/Register.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Register extends HttpServlet
+{
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+       String u = req.getParameter("uid");
+       String p = req.getParameter("pass");
+        PrintWriter out = res.getWriter(); // get the response writer for write onm response window
+        out.println("<body bgcolor=lightgreen><tt>");// html code aayega 
+        if(p.equals("hiet@"))
+        {
+           out.println("Hello "+u+",Welcome's U");
+           out.close();
+        }   
+        out.println("Sorry "+u+", this is Invalid User Id & Password, try again");
+        out.println("<br><a href=index.html>Click here for try again</a>");
+    }
+    
+}
+
+
+
